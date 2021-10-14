6a3d55ff2cddb22e7e23cac2a4921684085d4fca
Anthony B. Earles <tony@organicecommerce.com>
2021-10-14 16:33:54 +0100

    First commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ecrubox_Javascript',
+    __DIR__
+);
+
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+const newGtm = 'WeltPixel_GoogleTagManager/js/weltpixel_gtm';
+const newPl = 'WeltPixel_GoogleTagManager/js/weltpixel_persistentlayer';
+ var config = {
+         map: {
+            '*': {
+                weltpixel_gtm: newGtm,
+                weltpixel_persistentLayer: newPl
+            }
+        }
+};
